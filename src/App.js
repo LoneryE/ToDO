80b0a4a968c935f2cfd721b951a9a5a9f047d921
LoneryE <max.dobrikE@gmail.com>
2023-04-29 09:26:31 +0100

    design completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,77 @@
-function App() {
+import imgs from "./assets/images/img";
+
+const App = () => {
   return (
     <div className="App">
-      asd
+      <div className="menu">
+        <span className="menu_section">
+          <div className="menu_logo">
+            <h3><b>Menu</b></h3> 
+          </div>
+          <div className="menu_burger">
+            <img src={imgs.menu} alt="burger" width={25}/>
+          </div>
+        </span>
+        <div className="search">
+          <input type="text" placeholder="search"/>
+        </div>
+        <h5>TASKS</h5>
+        <div className="tasks_list">
+          <button className="tasks_today">Today</button>
+          <button className="tasks_sticky">Sticky Wall</button>
+        </div>
+        <h5>LISTS</h5>
+        <div className="lists_list">
+          <button className="list">Work</button>
+          <button className="add_list">Add New List</button>
+        </div>
+      </div>
+      <div className="wall">
+        <div className="wall_header">
+          <h1>
+            <b>Work</b>
+          </h1>
+          <div className="counter">
+            3
+          </div>
+        </div>
+        <div className="tasks">
+          <button className="add_task">Add New Task</button>
+          <div className="filters">
+            <button>name</button>
+            <button>completed</button>
+          </div>
+          <ul>
+            <li>
+              <div className="info_task">
+                <input type="checkbox"/>
+                task 1
+              </div>
+              <button className="delete_btn">
+                Delete
+              </button>
+            </li>
+            <li>
+              <div className="info_task">
+                <input type="checkbox"/>
+                task 2
+              </div>
+              <button className="delete_btn">
+                Delete
+              </button>
+            </li>
+            <li>
+              <div className="info_task">
+                <input type="checkbox"/>
+                task 3
+              </div>
+              <button className="delete_btn">
+                Delete
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/assets/images/img.js
@@ -0,0 +1,5 @@
+import menu from './menu.png'
+
+const imgs = {menu}
+
+export default imgs
